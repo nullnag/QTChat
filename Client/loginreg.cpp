8c49c03a03d0f03a3a77125836a0f4e4509afaf7
nullnag <153712889+nullnag@users.noreply.github.com>
2024-05-15 14:52:02 +0300

    Add files via upload

---
+++ Client/loginreg.cpp
@@ -0,0 +1,84 @@
+#include "loginreg.h"
+#include "ui_loginreg.h"
+#include <reg.h>
+
+#include <mainwindow.h>
+LoginReg::LoginReg(QWidget *parent)
+
+    : QDialog(parent)
+    , ui(new Ui::LoginReg)
+{
+    ui->setupUi(this);
+    socket = new QTcpSocket(this);
+    socket->connectToHost("26.103.198.212",4444);
+    qDebug() << socket;
+    connect(socket,&QTcpSocket::readyRead,this, &LoginReg::sockReady);
+    connect(socket,&QTcpSocket::disconnected,this, &QTcpSocket::deleteLater);
+    registry = new Reg(socket,this);
+
+}
+
+LoginReg::~LoginReg()
+{
+    qDebug() << "LoginReg";
+    delete ui;
+}
+
+void LoginReg::on_SignUpButton_clicked()
+{
+    registry->show();
+    registry->exec();
+}
+
+void LoginReg::sockReady(){
+    Data.clear();
+    Data = socket->readAll();
+    QDataStream stream(&Data,QIODevice::ReadOnly);
+    bool check;
+    qint32 requestNum;
+    stream >> check >> requestNum;
+    qDebug() << requestNum;
+    if  (check == 1 && requestNum == 3){
+        registry->check = 1;
+    }
+    if (check == 1 && requestNum == 1){
+        QMessageBox::information(nullptr,"Успех","Все верно");
+        close();
+
+
+        MainWindow *Main = new MainWindow;
+        Main->show();
+    }
+    else if (check == 0 && requestNum == 1){
+        QMessageBox::information(nullptr,"Ошибка","Неверный логин либо пароль");
+    }
+}
+
+
+void LoginReg::on_SignInButton_clicked()
+{
+    if (socket->waitForConnected()){
+        Data.clear();
+        QDataStream stream(&Data, QIODevice::WriteOnly);
+        stream << 1 << this->Login << this->Password;
+        socket->write(Data);
+        socket->flush();
+        socket->waitForBytesWritten();
+    }
+    else{
+        qDebug() << "error";
+    }
+}
+
+
+void LoginReg::on_LoginEdit_textChanged(const QString &arg1)
+{
+    this->Login = arg1;
+}
+
+
+void LoginReg::on_PasswordEdit_textChanged(const QString &arg1)
+{
+    this->Password = arg1;
+}
+
