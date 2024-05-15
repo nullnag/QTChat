c557af242a9f769b63d3550679f2f9e129203263
nullnag <153712889+nullnag@users.noreply.github.com>
2024-05-15 14:48:53 +0300

    Add files via upload

---
+++ Client/mainwindow.cpp
@@ -0,0 +1,74 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    socket = new QTcpSocket(this);
+    connect(socket, &QTcpSocket::readyRead,this, &MainWindow::sockReady);
+    connect(socket,&QTcpSocket::disconnected,socket, &QTcpSocket::deleteLater);
+    nextBlockSize = 0;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    socket->connectToHost("26.103.198.212",5555);
+}
+
+void MainWindow::SendToServer(QString str)
+{
+    Data.clear();
+    QDataStream out(&Data, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Version());
+    out << quint16(0) << str;
+    out.device()->seek(0);
+    out << quint16(Data.size() - sizeof(quint16));
+    socket->write(Data);
+    ui->lineEdit->clear();
+}
+
+void MainWindow::sockReady()
+{
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Version());
+    if(in.status() == QDataStream::Ok){
+        for (;;){
+            if (nextBlockSize == 0){
+                if (socket->bytesAvailable() < 2){
+                    break;
+                }
+                in >> nextBlockSize;
+            }
+            if (socket->bytesAvailable() < nextBlockSize){
+                break;
+            }
+            QString str;
+            in >> str;
+            nextBlockSize = 0;
+            ui->textBrowser->append(str);
+        }
+    }
+    else{
+        ui->textBrowser->append("error");
+    }
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    SendToServer(ui->lineEdit->text());
+}
+
+
+void MainWindow::on_lineEdit_returnPressed()
+{
+    SendToServer(ui->lineEdit->text());
+}
+
