6851fc14d817f2de9108742664bf75c96c4c92b2
nullnag <153712889+nullnag@users.noreply.github.com>
2024-07-02 18:43:20 +0300

    Delete Server/Server/Server directory

--- Server/Server/Server/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QCoreApplication>
-#include <server.h>
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    Server server;
-    DBServer dbserver;
-    return a.exec();
-}
